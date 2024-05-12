9e71fb7b245a5b787e1b5725f1d2789aba623ca1
2018yuli <l898974766@outlook.com>
2024-05-12 22:12:09 +0800

    20240512

--- learn2/src/main.rs
+++ learn2/src/main.rs
@@ -8,17 +8,19 @@ use components::camera::CameraPlugin;
 
 fn main() {
     App::new()
-    // build-ins
-    .insert_resource(ClearColor(Color::rgb(1.0, 1.0, 1.0)))
-    .insert_resource(AmbientLight {
-        color: Color::default(),
-        brightness: 0.95,
-    })
     .add_plugins(DefaultPlugins)
+    // build-ins
+    .insert_resource(ClearColor(Color::rgb(0.49, 0.44, 0.46)))
+    .insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.75,
+    })
+    
     // 用户插件
     .add_plugins(MovementPlugin)
     .add_plugins(DebugPlugin)
     .add_plugins(SpaceshipPlugin)
     .add_plugins(CameraPlugin)
     .run();
+    
 }
